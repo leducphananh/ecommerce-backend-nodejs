84015ca533f5887853609f88b6acc996a289761d
AnhLDP <leducphananh@gmail.com>
2025-01-20 11:04:53 +0700

    feat: connect mongodb

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const compression = require("compression");
 const express = require("express");
-const { default: helmet } = require("helmet");
+const helmet = require("helmet");
 const morgan = require("morgan");
 const app = express();
 
@@ -10,6 +10,9 @@ app.use(helmet());
 app.use(compression());
 
 // init db
+require("./dbs/init.mongodb");
+// const { checkOverload } = require("./helpers/check.connect");
+// checkOverload();
 
 // init routes
 app.get("/", (req, res, next) => {
---
+++ src/dbs/init.mongodb.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { countConnect } = require("../helpers/check.connect");
+
+const connectString = "mongodb://localhost:27017/shopDEV";
+
+class Database {
+  constructor() {
+    this.connect();
+  }
+
+  connect(type = "mongodb") {
+    // dev
+    if (1 === 1) {
+      mongoose.set("debug", true);
+      mongoose.set("debug", {
+        color: true,
+      });
+    }
+
+    mongoose
+      .connect(connectString)
+      .then(() => {
+        console.log("Connected Mongodb Success!");
+        countConnect();
+      })
+      .catch((err) => console.log("Error Connect!"));
+  }
+
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+
+    return Database.instance;
+  }
+}
+
+const instanceMongodb = Database.getInstance();
+
+module.exports = instanceMongodb;
